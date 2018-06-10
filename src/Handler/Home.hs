f298e5c82785ad786a61a862b67edf8e52610a85
Ajit Nath <nath.ajit01@gmail.com>
2018-06-10 20:17:35 +0800

    Change content

--- src/Handler/Home.hs
+++ src/Handler/Home.hs
@@ -32,7 +32,7 @@ getHomeR = do
     defaultLayout $ do
         let (commentFormId, commentTextareaId, commentListId) = commentIds
         aDomId <- newIdent
-        setTitle "Welcome To Yesod!"
+        setTitle "Ajit Nath's Blog"
         $(widgetFile "homepage")
 
 postHomeR :: Handler Html
